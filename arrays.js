edf039be6d076f0dd4b2d963ec995d823d4d8fb3
Khushboo Gupta <khushboogupta.bu@gmail.com>
2019-12-16 12:33:06 -0700

    added files

--- arrays.js
+++ arrays.js
@@ -2,6 +2,15 @@ var counter = function(a){
       return ('the array is ' + a + ' and the length is ' + a.length)
 }
 
-a=['khushboo','gupta','india']
-console.log(counter(a));
+// a=['khushboo','gupta','india']
+// console.log(counter(a));
 
+var adder = function(a,b){
+      return 'the sum is ${a+b}'
+}
+
+var pi = 3.42
+
+module.exports.counter=counter
+module.exports.adder=adder
+module.exports.pi=pi
---
+++ require.js
@@ -0,0 +1,5 @@
+var counter = require('./arrays')
+a=['khushboo','gupta','india']
+console.log(arrays.counter(a))
+console.log(arrays.adder(10,20))
+console.log(arrays.pi)
